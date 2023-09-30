9db1e60416806fb4c426dbe904ea98eff60621d8
S E Prince Joseph <sindala.prince@gmail.com>
2023-09-30 22:09:15 +0530

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+var grades = document.querySelectorAll(".grades-input");
+var credits = document.querySelectorAll(".credits-input");
+var data = document.querySelector("#submit");
+var num=0, denom=0, result = 0;
+var tc = document.getElementById("tc-cont");
+var gr = document.getElementById("gpa-cont");
+
+data.addEventListener("click",onclick);
+
+function onclick(){
+
+    for(let i=0;i<grades.length;i++){
+        if((grades[i].value) != "" || credits[i].value != ""){
+            num += (Number(grades[i].value))*(Number(credits[i].value));
+            denom += (Number(credits[i].value));
+        }
+    }
+
+    
+    const result = (num/denom);
+    const fresult = result.toFixed(2);
+    tc.innerHTML = denom;
+    gr.innerHTML = fresult;
+
+}
+
+
